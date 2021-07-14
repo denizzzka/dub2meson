ca6e94abf70052322b69dedf6e902ea5351f3355
Denis Feklushkin <feklushkin.denis@gmail.com>
2021-07-15 01:49:29 +0700

    tabs -> spaces

--- source/processing.d
+++ source/processing.d
@@ -9,18 +9,18 @@ Dub createDub(in Cfg cfg)
 {
     //TODO: add package_suppliers and options
 
-	Dub dub;
+    Dub dub;
 
     if(cfg.bare)
     {
-		import std.file: getcwd;
+        import std.file: getcwd;
 
         dub = new Dub(NativePath(getcwd()));
-	}
+    }
     else
-		dub = new Dub(cfg.rootPath);
+        dub = new Dub(cfg.rootPath);
 
-	dub.rootPath = NativePath(cfg.rootPath);
+    dub.rootPath = NativePath(cfg.rootPath);
     dub.dryRun = cfg.annotate;
     dub.defaultPlacementLocation = cfg.placementLocation;
 
@@ -37,29 +37,29 @@ void fetchAllNonOptionalDependencies(Dub dub)
 
 void createMesonFiles(Dub dub, in Cfg cfg)
 {
-	import std.file: mkdirRecurse;
+    import std.file: mkdirRecurse;
 
-	byte[string] processedPackages;
+    byte[string] processedPackages;
 
     foreach(currPkg; dub.project.getTopologicalPackageList)
     {
-		const basePkgName = currPkg.basePackage.name;
+        const basePkgName = currPkg.basePackage.name;
 
-		if(basePkgName !in processedPackages)
-		{
-			if(cfg.verbose)
-			{
-				import std.stdio;
+        if(basePkgName !in processedPackages)
+        {
+            if(cfg.verbose)
+            {
+                import std.stdio;
 
-				writefln(`Processing '%s' (%s)`,
-					basePkgName,
-					currPkg.basePackage.recipe.version_,
-				);
-			}
+                writefln(`Processing '%s' (%s)`,
+                    basePkgName,
+                    currPkg.basePackage.recipe.version_,
+                );
+            }
 
-			createMesonFile(currPkg.basePackage, cfg);
-			processedPackages[basePkgName] = 1;
-		}
+            createMesonFile(currPkg.basePackage, cfg);
+            processedPackages[basePkgName] = 1;
+        }
     }
 }
 
@@ -69,202 +69,202 @@ import std.stdio;
 
 void createMesonFile(in Package pkg, in Cfg cfg)
 {
-	import dub.internal.vibecompat.core.file;
+    import dub.internal.vibecompat.core.file;
 
-	immutable filename = `meson.build`;
+    immutable filename = `meson.build`;
 
-	const NativePath mesonBuildFilePath = pkg.path ~ filename;
+    const NativePath mesonBuildFilePath = pkg.path ~ filename;
 
-	if(mesonBuildFilePath.existsFile)
-	{
-		if(cfg.verbose)
-			writeln("file ", mesonBuildFilePath, " exists, skipping");
+    if(mesonBuildFilePath.existsFile)
+    {
+        if(cfg.verbose)
+            writeln("file ", mesonBuildFilePath, " exists, skipping");
 
-		return;
-	}
+        return;
+    }
 
-	const subprojects = NativePath(cfg.subprojectsPath);
+    const subprojects = NativePath(cfg.subprojectsPath);
 
-	auto meson_build = new RootMesonBuildFile(subprojects~`packagefiles`~(pkg.name~`_changes`)~filename);
+    auto meson_build = new RootMesonBuildFile(subprojects~`packagefiles`~(pkg.name~`_changes`)~filename);
 
-	// Adding project()
-	{
-		auto project = meson_build.rootSection.addSection(`project`, Bracket.ROUND);
-		project.addLine(pkg.basePackage.name.quote~`,`);
-		project.addLine(`['d'],`);
-		project.addKeyVal(`version`, pkg.basePackage.recipe.version_);
-		project.addKeyVal(`license`, pkg.basePackage.recipe.license);
-		project.addKeyVal(`meson_version`, `>=0.58.1`);
-		project.addArray(
-			`default_options`.keyword,
-			Bracket.SQUARE,
-			[
-				"FIXME".quote,
-				"FIXME".quote,
-			]
-		);
-	}
+    // Adding project()
+    {
+        auto project = meson_build.rootSection.addSection(`project`, Bracket.ROUND);
+        project.addLine(pkg.basePackage.name.quote~`,`);
+        project.addLine(`['d'],`);
+        project.addKeyVal(`version`, pkg.basePackage.recipe.version_);
+        project.addKeyVal(`license`, pkg.basePackage.recipe.license);
+        project.addKeyVal(`meson_version`, `>=0.58.1`);
+        project.addArray(
+            `default_options`.keyword,
+            Bracket.SQUARE,
+            [
+                "FIXME".quote,
+                "FIXME".quote,
+            ]
+        );
+    }
 
-	//Collect source files
-	void collect(in string[][string] searchPaths, MesonBuildFile.CollectType ct, string typeName, string wildcard, bool isSourceFiles = false)
-	{
-		import dub_stuff.collect: collectFiles;
+    //Collect source files
+    void collect(in string[][string] searchPaths, MesonBuildFile.CollectType ct, string typeName, string wildcard, bool isSourceFiles = false)
+    {
+        import dub_stuff.collect: collectFiles;
 
-		auto collected = collectFiles(pkg.path, searchPaths, wildcard);
+        auto collected = collectFiles(pkg.path, searchPaths, wildcard);
 
-		foreach(suffix, files; pkg.recipe.buildSettings.sourceFiles)
-			collected[suffix] ~= files;
+        foreach(suffix, files; pkg.recipe.buildSettings.sourceFiles)
+            collected[suffix] ~= files;
 
-		foreach(suffix, ref paths; collected)
-		{
-			const underscore = (suffix == "") ? "" : "_";
+        foreach(suffix, ref paths; collected)
+        {
+            const underscore = (suffix == "") ? "" : "_";
 
-			if(ct == MesonBuildFile.CollectType.IncludeDirs)
-			{
-				// remove file part and '/' from paths
-				foreach(ref p; paths)
-					p = NativePath(p).parentPath.toString[0 .. $-1];
-			}
+            if(ct == MesonBuildFile.CollectType.IncludeDirs)
+            {
+                // remove file part and '/' from paths
+                foreach(ref p; paths)
+                    p = NativePath(p).parentPath.toString[0 .. $-1];
+            }
 
-			import std.algorithm.sorting: sort;
-			import std.algorithm.iteration: uniq;
-			import std.array: array;
+            import std.algorithm.sorting: sort;
+            import std.algorithm.iteration: uniq;
+            import std.array: array;
 
-			paths = paths.sort.uniq.array;
+            paths = paths.sort.uniq.array;
 
-			meson_build.addFilesToFilesArrays(ct, suffix~underscore~typeName, paths);
-		}
-	}
+            meson_build.addFilesToFilesArrays(ct, suffix~underscore~typeName, paths);
+        }
+    }
 
-	{
-		const bs = pkg.recipe.buildSettings;
+    {
+        const bs = pkg.recipe.buildSettings;
 
-		collect(bs.importPaths, MesonBuildFile.CollectType.IncludeDirs, `include`, `*.{d,di}`);
-		collect(bs.sourcePaths, MesonBuildFile.CollectType.Files, `sources`, `*.d`, true);
-		collect(bs.stringImportPaths, MesonBuildFile.CollectType.StringArray, `string_imports`, "*");
-	}
+        collect(bs.importPaths, MesonBuildFile.CollectType.IncludeDirs, `include`, `*.{d,di}`);
+        collect(bs.sourcePaths, MesonBuildFile.CollectType.Files, `sources`, `*.d`, true);
+        collect(bs.stringImportPaths, MesonBuildFile.CollectType.StringArray, `string_imports`, "*");
+    }
 
-	// Loop over configurations
-	{
-		foreach(const conf; pkg.recipe.configurations)
-		{
-			import std.conv: to;
-			import dub.recipe.packagerecipe;
-			import dub.compilers.buildsettings: TargetType;
+    // Loop over configurations
+    {
+        foreach(const conf; pkg.recipe.configurations)
+        {
+            import std.conv: to;
+            import dub.recipe.packagerecipe;
+            import dub.compilers.buildsettings: TargetType;
 
-			BuildOptions bo;
+            BuildOptions bo;
 
-			with(bo)
-			with(TargetType)
-			switch(conf.buildSettings.targetType)
-			{
-				case none:
-					buildSomeBinary = false;
-					continue; // Just ignore whole package
+            with(bo)
+            with(TargetType)
+            switch(conf.buildSettings.targetType)
+            {
+                case none:
+                    buildSomeBinary = false;
+                    continue; // Just ignore whole package
 
-				case sourceLibrary:
-					buildSomeBinary = false;
-					break;
+                case sourceLibrary:
+                    buildSomeBinary = false;
+                    break;
 
-				case executable:
-					buildExecutable = true;
-					break;
+                case executable:
+                    buildExecutable = true;
+                    break;
 
-				case library:
-					buildLibrary = true;
-					break;
+                case library:
+                    buildLibrary = true;
+                    break;
 
-				case autodetect:
-					buildExecutable = true;
-					buildLibrary = true;
-					break;
+                case autodetect:
+                    buildExecutable = true;
+                    buildLibrary = true;
+                    break;
 
-				case dynamicLibrary:
-					forceDynamicLib = true;
-					break;
+                case dynamicLibrary:
+                    forceDynamicLib = true;
+                    break;
 
-				case staticLibrary:
-					forceStaticLib = true;
-					break;
+                case staticLibrary:
+                    forceStaticLib = true;
+                    break;
 
-				default:
-					enforce(false, pkg.basePackage.name~`: unsupported target type: `~conf.buildSettings.targetType.to!string);
-					return;
-			}
+                default:
+                    enforce(false, pkg.basePackage.name~`: unsupported target type: `~conf.buildSettings.targetType.to!string);
+                    return;
+            }
 
-			// processExecOrLib can process both executable() and library():
-			if(bo.buildExecutable)
-				processExecOrLib(meson_build, conf, pkg, bo);
+            // processExecOrLib can process both executable() and library():
+            if(bo.buildExecutable)
+                processExecOrLib(meson_build, conf, pkg, bo);
 
-			if(bo.buildLibrary)
-				processExecOrLib(meson_build, conf, pkg, bo);
+            if(bo.buildLibrary)
+                processExecOrLib(meson_build, conf, pkg, bo);
 
-			conf.buildSettings.targetType.writeln;
-		}
-	}
+            conf.buildSettings.targetType.writeln;
+        }
+    }
 
-	meson_build.path.writeln;
-	meson_build.writeln;
-	//~ pkg.recipe.configurations.writeln;
-	//~ pkg.recipe.buildSettings.writeln;
+    meson_build.path.writeln;
+    meson_build.writeln;
+    //~ pkg.recipe.configurations.writeln;
+    //~ pkg.recipe.buildSettings.writeln;
 }
 
 struct BuildOptions
 {
-	bool buildSomeBinary = true;
-	bool buildExecutable;
-	bool buildLibrary;
-	bool forceDynamicLib;
-	bool forceStaticLib;
+    bool buildSomeBinary = true;
+    bool buildExecutable;
+    bool buildLibrary;
+    bool forceDynamicLib;
+    bool forceStaticLib;
 }
 
 void processSourceFiles(MesonBuildFile meson_build, in ConfigurationInfo conf, in Package pkg, in BuildOptions bo)
 {
-	import dub_stuff.collect;
+    import dub_stuff.collect;
 
-	conf.writeln;
+    conf.writeln;
 
-	//~ auto tree = collectFiles(srcPaths, "*.d");
+    //~ auto tree = collectFiles(srcPaths, "*.d");
 }
 
 import dub.recipe.packagerecipe: ConfigurationInfo;
 
 void processExecOrLib(RootMesonBuildFile meson_build, in ConfigurationInfo conf, in Package pkg, in BuildOptions bo)
 {
-	string name;
-	string suffix;
+    string name;
+    string suffix;
 
-	if(bo.buildExecutable)
-	{
-		name = `executable`;
-		suffix = `_exe`;
-	}
-	else if(bo.buildLibrary)
-	{
-		name = `library`;
-		suffix = `_lib`;
-	}
-	else
-		assert(false);
+    if(bo.buildExecutable)
+    {
+        name = `executable`;
+        suffix = `_exe`;
+    }
+    else if(bo.buildLibrary)
+    {
+        name = `library`;
+        suffix = `_lib`;
+    }
+    else
+        assert(false);
 
-	//~ conf.writeln;
+    //~ conf.writeln;
 
-	import std.algorithm.iteration: map;
-	import std.array: array;
+    import std.algorithm.iteration: map;
+    import std.array: array;
 
-	const depsList = pkg.getDependencies(conf.name).byKey.array;
+    const depsList = pkg.getDependencies(conf.name).byKey.array;
 
-	foreach(ref e; depsList)
-		meson_build.addDependency(e);
+    foreach(ref e; depsList)
+        meson_build.addDependency(e);
 
-	auto exeOrLib = meson_build.rootSection.addSection(conf.name~suffix~` = `~name, Bracket.ROUND);
-	exeOrLib.addLine(conf.name.quote~`,`);
+    auto exeOrLib = meson_build.rootSection.addSection(conf.name~suffix~` = `~name, Bracket.ROUND);
+    exeOrLib.addLine(conf.name.quote~`,`);
 
-	auto deps = exeOrLib.addArray(
-		`dependencies`.keyword,
-		Bracket.SQUARE,
-		depsList.map!(a => a~`_dep`).array
-	);
+    auto deps = exeOrLib.addArray(
+        `dependencies`.keyword,
+        Bracket.SQUARE,
+        depsList.map!(a => a~`_dep`).array
+    );
 
-	writeln("dependencies >>>>>>>>>>>> ", pkg.getDependencies(conf.name));
+    writeln("dependencies >>>>>>>>>>>> ", pkg.getDependencies(conf.name));
 }
